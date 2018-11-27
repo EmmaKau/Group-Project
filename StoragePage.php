f70b5bf3a46ceafc524f0b62e1bcd62766ffdc6c
EmmaKau <emmakauzmann@QLP.localdomain>
2018-11-26 17:31:27 -0800

    first commit :)

---
+++ StoragePage.php
@@ -0,0 +1,20 @@
+ 
+<!DOCTYPE html>
+
+<html>
+<body>
+    
+    <center>  Here is your constructive comment: </center> 
+    <?php
+
+        require "CommentPage.php";
+        echo $_GET["comment"]; 
+        ?>
+
+  
+    
+
+</body>
+</html>
+
+        
