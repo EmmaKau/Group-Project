ab62dfe60e15c165c1824c0528b579c53a8b7b27
smcclain56 <43018457+smcclain56@users.noreply.github.com>
2018-12-05 11:58:46 -0800

    Sarah McClain

--- autocomplete.php
+++ autocomplete.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
+This file is the the  main part of the search bar. It is how the autocomplete search bar works.
+
+Author: Sarah McClain
 -->
 <?php
 
